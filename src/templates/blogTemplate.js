adeefb5348a1d03d3ef1559454afceaeb3668742
viral <pviral810@gmail.com>
2019-03-03 13:55:53 -0500

    Fix disqus embed code by using gatsby browser api

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,25 +3,6 @@ import { graphql } from "gatsby"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 
-const disqusCode = () =>
-  /**
-   *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-   *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-  /*
-  var disqus_config = function () {
-  this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-  this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-  };
-  */
-  (function() {
-    // DON'T EDIT BELOW THIS LINE
-    var d = document,
-      s = d.createElement("script")
-    s.src = "https://https-viralpatel-blog.disqus.com/embed.js"
-    s.setAttribute("data-timestamp", +new Date())
-    ;(d.head || d.body).appendChild(s)
-  })()
-
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
@@ -73,7 +54,6 @@ export default function Template({
       <br />
       <br />
       <div id="disqus_thread" />
-      <script type="text/javascript" dangerouslySetInnerHTML={disqusCode()} />
     </Layout>
   )
 }
