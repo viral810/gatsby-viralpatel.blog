220a95d6c90fc2943283d3c25d8f3c5570f17264
viral <pviral810@gmail.com>
2019-04-25 11:27:43 -0400

    Test PathFactory website library script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -28,5 +28,25 @@ const disqusEmbedCode = () =>
 exports.onRouteUpdate = ({ location }) => {
   var dialog = document.querySelector("dialog")
   if (dialog) window.dialogPolyfill.registerDialog(dialog)
-  if (document.getElementById("disqus_thread")) disqusEmbedCode()
+  if (document.getElementById("disqus_thread"))
+    disqusEmbedCode()(function(j, u, k, e, b, o, x) {
+      ;(j[b] =
+        j[b] ||
+        function() {
+          ;(j[b].q = j[b].q || []).push(arguments)
+        }),
+        (j[b].l = 1 * new Date())
+      ;(o = u.createElement(k)), (x = u.getElementsByTagName(k)[0])
+      o.async = 1
+      o.src = e
+      x.parentNode.insertBefore(o, x)
+    })(
+      window,
+      document,
+      "script",
+      "https://app.cdn.lookbookhq.com/staging2/jukebox/current/WebsiteTracking.js",
+      "lbhq"
+    )
+  lbhq("create", "LB-5B4D1EC2-1")
+  lbhq("_env", "staging2")
 }
