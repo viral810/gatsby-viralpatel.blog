540e3aa09be4d1b565d4bc53ba6f744947341e3b
Viral Patel <pviral810@gmail.com>
2020-07-28 13:45:06 -0400

    Install gatsby-plugin-advanced-sitemap plugin to generate good sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-google-analytics`,
